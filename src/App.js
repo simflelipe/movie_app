6357c274baa29309ca9ebcb0c78d7550cb29ffad
simflelipe <simflelipe@gmail.com>
2020-08-07 05:07:59 +0000

    About State

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  state = {
+    count: 0
+  };
+
+
+  //function
+  add = () => {
+    console.log("add");
+    this.setState(current => ({ count : current.count + 1 }));
+  };
+
+  minus = () => {
+    console.log("minus");
+    this.setState(current => ({ count : current.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
